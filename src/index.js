4d527caa9a301051311b3ebde71601ca2e0e30b3
varunyk1805 <varunyk1805@gmail.com>
2022-01-18 09:27:57 +0200

    add initialData

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import './js/movies';
 import './js/watched_queued_movies';
 import './js/header';
 import './js/trendingMovies';
-import './js/creadCard'
-import './js/modalOpenClose'
-import './js/seachMovies'
+import './js/creadCard';
+import './js/modalOpenClose';
+import './js/seachMovies';
+// якщо розкоментувати initialData, то потрібно
+// закоментувати trendingMovies і розкоментувати constants
+
+// import './js/initialData';
+
+// якщо розкоментувати constants, то потрібно
+// закоментувати trendingMovies і розкоментувати initialData
+
+// import './js/constants';
 
 // DATA_URL = 'https://team-project-1da18-default-rtdb.europe-west1.firebasedatabase.app/user.json';  // Путь к данным пользователя в базе данных
 // const KEY = '2cf91cf1fed5026ae9524dc97ad33068';  // Ключ доступа к API фильмов
---
+++ src/js/constants.js
@@ -0,0 +1,4 @@
+const KEY = '2cf91cf1fed5026ae9524dc97ad33068';
+const MOVIES_SET = document.querySelector('.section-movies__set');
+
+export { KEY, MOVIES_SET };
