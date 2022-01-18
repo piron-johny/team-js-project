8bae006fda7f19d45bcd533222489a3ca39db656
Oleksii Ostafiichuk <issimo333@gmail.com>
2022-01-18 18:33:51 +0200

    пагинация для главной страницы

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,57 +1,52 @@
-// 1. Пагинация с помощью плагина tui-pagination https://www.npmjs.com/package/tui-pagination
+// // 1. Пагинация с помощью плагина tui-pagination https://www.npmjs.com/package/tui-pagination
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 
+import moviesRender from '../hbs/render.hbs';
 import FetchServise from './servisesAPI.js';
 const fetchServise = new FetchServise();
 
-import renderMoviesSet from './seachMovies.js';
-// import fetchTopFilms from './watched_queued_movies';
-import fetchMovies from './seachMovies';
-
-const paginationEl = document.getElementById('tui-pagination-container');
+const paginationEl = document.getElementById('pagination');
 const galleryEl = document.querySelector('.section-movies__set');
 // console.log(galleryEl);
 
-function tuiPagination() {
-  const options = {
-    totalItems: 400,
-    itemsPerPage: 20,
-    visiblePages: 5,
-    page: 1,
-    centerAlign: true,
-    firstItemClassName: 'tui-first-child',
-    lastItemClassName: 'tui-last-child',
-    // template: {
-    //   page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    //   currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    //   moveButton:
-    //     '<a href="#" class="tui-page-btn tui-{{type}}">' +
-    //     '<span class="tui-ico-{{type}}">{{type}}</span>' +
-    //     '</a>',
-    //   disabledMoveButton:
-    //     '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-    //     '<span class="tui-ico-{{type}}">{{type}}</span>' +
-    //     '</span>',
-    //   moreButton:
-    //     '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-    //     '<span class="tui-ico-ellip">...</span>' +
-    //     '</a>',
-    // },
-    usageStatistics: false,
-  };
+const options = {
+  totalItems: 400,
+  itemsPerPage: 20,
+  visiblePages: 5,
+  page: 1,
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  usageStatistics: false,
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+};
+const pagination = new Pagination(paginationEl, options);
+pagination.getCurrentPage();
 
-  const pagination = new Pagination('pagination', options);
-
-  pagination.on('beforeMove', function (eData) {
-    fetchServise.page = eData.page;
-    // console.log(fetchServise.page);
-    fetchServise.movies().then(films => {
-      galleryEl.innerHTML = '';
-      // renderMoviesSet(films.data.results);
-      console.log(films.data.results);
-    });
+pagination.on('afterMove', eData => {
+  const currentPage = eData.page;
+  // console.log(currentPage);
+  return fetchServise.movies(currentPage).then(data => {
+    galleryEl.innerHTML = '';
+    galleryEl.insertAdjacentHTML('afterbegin', moviesRender(data.data.results));
+    // moviesRender(data);
+    // console.log(data.data.results);
+    return;
   });
-}
-
-export { tuiPagination };
+});
