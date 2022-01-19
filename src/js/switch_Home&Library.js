e23a5ad104bcce032ded2de07db2eb033bf78762
Vitalii Varunyk <86269556+varunyk1805@users.noreply.github.com>
2022-01-19 16:23:47 +0200

    Update switch_Home&Library.js
    Додав на лого загрузку домашньої сторінки

--- src/js/switch_Home&Library.js
+++ src/js/switch_Home&Library.js
@@ -1,18 +1,22 @@
 import moviesRender from '../hbs/render.hbs';
 
-const btnMyLibrary = document.querySelector('#library');
+const logo = document.querySelector('.page-header__logo');
 const btnHome = document.querySelector('#home');
+const btnMyLibrary = document.querySelector('#library');
 
 const moviesSet = document.querySelector('.section-movies__set');
 
-const renderMyLibarary = () => {
+const renderMyLibarary = event => {
+    event.preventDefault();
     const libaryMarkup = moviesRender(JSON.parse(localStorage.getItem('Watched')));
     moviesSet.innerHTML = libaryMarkup;
 }
-const renderHome = () => {
+const renderHome = event => {
+    event.preventDefault();
     const libaryMarkup = moviesRender(JSON.parse(localStorage.getItem('Trending')));
     moviesSet.innerHTML = libaryMarkup;
 }
 
-btnMyLibrary.addEventListener('click', renderMyLibarary);
+logo.addEventListener('click', renderHome);
 btnHome.addEventListener('click', renderHome);
+btnMyLibrary.addEventListener('click', renderMyLibarary);
