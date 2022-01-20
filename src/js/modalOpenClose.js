bea44d2739c2b5f883a2646617b21f4f730e16fc
Piron-Johny <piron.johny@gmail.comm>
2022-01-20 19:16:27 +0200

    убрал ошибку. Закомментировал логи

--- src/js/modalOpenClose.js
+++ src/js/modalOpenClose.js
@@ -1,11 +1,10 @@
 import modalRender from '../hbs/modalRender.hbs';
 
-const body = document.querySelector("body");
-const btnCloseModal = document.querySelector("[data-modal-close]");
-const backdropModalEl = document.querySelector("[data-backdrop]");
-export const cardOpenModal = document.querySelector(".section-movies__set");
+const body = document.querySelector('body');
+const btnCloseModal = document.querySelector('[data-modal-close]');
+const backdropModalEl = document.querySelector('[data-backdrop]');
+const cardOpenModal = document.querySelector('.section-movies__set');
 const modalEl = document.querySelector('.modal__card');
-
 
 cardOpenModal.addEventListener('click', onModalOpen);
 btnCloseModal.addEventListener('click', onModalClose);
@@ -15,35 +14,40 @@ backdropModalEl.addEventListener('click', onBackdropClick);
 window.addEventListener('keydown', onEscPress);
 
 function onModalOpen(e) {
+  if (
+    e.target.parentElement.className === 'section-movies__card' ||
+    e.target.parentElement.className === 'movies-card__genres-list'
+  ) {
+    backdropModalEl.classList.remove('is-hidden');
+    body.style.overflow = 'hidden';
 
-      if (e.target.parentElement.className === 'section-movies__card' || e.target.parentElement.className === 'movies-card__genres-list')  {
-          backdropModalEl.classList.remove("is-hidden")
-          body.style.overflow = 'hidden'
-          
-          const findId = e.target.parentNode.id
-          const savedFilms = JSON.parse(localStorage.getItem(`Trending`));
-          const modalFilm = savedFilms.find(film => film.id === +findId)
+    const findId = e.target.parentNode.id;
+    const savedFilms = JSON.parse(localStorage.getItem(`Trending`));
+    const modalFilm = savedFilms.find(film => film.id === +findId);
 
-        modalEl.insertAdjacentHTML('beforeend', modalRender(modalFilm));
-    }     
+    modalEl.insertAdjacentHTML('beforeend', modalRender(modalFilm));
+  }
 }
 
 export default function onModalClose() {
-    backdropModalEl.classList.add("is-hidden")
-    modalEl.innerHTML = ''
+  backdropModalEl.classList.add('is-hidden');
+  body.style.overflow = 'visible';
+  modalEl.innerHTML = '';
 }
 
-function onBackdropClick(e){
-    if (e.currentTarget === e.target) {
-        onModalClose()
-    }
+function onBackdropClick(e) {
+  if (e.currentTarget === e.target) {
+    body.style.overflow = 'visible';
+    onModalClose();
+  }
 }
 
 function onEscPress(e) {
-    if (e.key === 'Escape') {
-        onModalClose()
-        window.removeEventListener('keydown', onModalClose);
- }
+  if (e.key === 'Escape') {
+    onModalClose();
+    body.style.overflow = 'visible';
+    window.removeEventListener('keydown', onModalClose);
+  }
 }
 
-export { cardOpenModal }
+export { cardOpenModal };
--- src/js/watched_queued_movies.js
+++ src/js/watched_queued_movies.js
@@ -4,40 +4,33 @@ import API from './trendingMovies';
 import { cardOpenModal } from './modalOpenClose';
 const axios = require('axios');
 
-const dataDiv = document.querySelector('.movie_data')
-
-
-
+const dataDiv = document.querySelector('.movie_data');
 
 async function fetchMovies() {
-    const response = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=3c97a1babd597f31c1fa5b3567357dfb`)
-    const data = await response.json();
-    // console.log(data.results)
-    dataDiv.insertAdjacentHTML('afterbegin', moviesRender(data.results));
+  const response = await fetch(
+    `https://api.themoviedb.org/3/trending/all/day?api_key=3c97a1babd597f31c1fa5b3567357dfb`,
+  );
+  const data = await response.json();
+  // console.log(data.results)
+  dataDiv.insertAdjacentHTML('afterbegin', moviesRender(data.results));
 
-    // getClickedElem()
-    getButtonWatched()
-    getButtonQueued()
-    return data;
+  // getClickedElem()
+  getButtonWatched();
+  getButtonQueued();
+  return data;
 }
-
-
-
 
 function addArrayToLocalStorage(array) {
-    localStorage.setItem('Watched', JSON.stringify(array))
-    localStorage.setItem('Queued', JSON.stringify(array))
-
+  localStorage.setItem('Watched', JSON.stringify(array));
+  localStorage.setItem('Queued', JSON.stringify(array));
 }
-fetchMovies(550)
+fetchMovies(550);
 
-
-addArrayToLocalStorage([])
+addArrayToLocalStorage([]);
 // function addMovieToWatched(e) {
 //     e.preventDefault();
 //     console.log(e)
 //     fetchMovies(550).then(data => {
-
 
 //         data.results.forEach(elem => {
 //             console.log(+e.srcElement.parentElement.attributes.value.value === elem.id);
@@ -49,7 +42,6 @@ addArrayToLocalStorage([])
 //                     parsedData.push(elem)
 //                     localStorage.setItem('Watched', JSON.stringify(parsedData))
 
-
 //                 } catch (error) {
 //                     console.log(error)
 //                 }
@@ -59,122 +51,97 @@ addArrayToLocalStorage([])
 
 //     })
 
-
 // }
 // console.log(cardOpenModal)
 
 async function defineMovie(e) {
-    e.preventDefault()
-    //  console.log(+e.target.parentElement.id)
-    const data = await API.trendingMovies();
-    const savedData = localStorage.getItem('Watched')
-    const parsedData = JSON.parse(savedData)
-    data.results.forEach(el => {
-        console.log(e)
-        console.log(el)
-        if (el.id === +e.target.parentElement.id) {
-                parsedData.push(el)
-                localStorage.setItem('Watched', JSON.stringify(parsedData))
-         
-            }
-        })
-
-        // if (parsedData.includes(el.id)) {
-        //     console.log(parsedData.includes(el.id))
-        //     // parsedData.splice(neededEl, 1)
-        //     console.log("Содержит")
-        // }
-
-
-        //    if (+e.target.parentElement.id === el.id) {
-        //             // parsedData.splice(neededEl, 1)
-        //        return
-        //       console.log(+e.target.parentElement.id === elem.id)
-        //         } 
-        //    } )
-        //     console.log(elem)
-        //     
-        //     
-
-        // })
-
-
-
-
-
-        // e.target.textContent = 'Remove from "Watched"'
-        // +e.target.parentElement.id !== i.id
-
-
-
-
+  e.preventDefault();
+  //  console.log(+e.target.parentElement.id)
+  const data = await API.trendingMovies();
+  const savedData = localStorage.getItem('Watched');
+  const parsedData = JSON.parse(savedData);
+  data.results.forEach(el => {
+    // console.log(e)
+    // console.log(el)
+    if (el.id === +e.target.parentElement.id) {
+      parsedData.push(el);
+      localStorage.setItem('Watched', JSON.stringify(parsedData));
     }
+  });
 
+  // if (parsedData.includes(el.id)) {
+  //     console.log(parsedData.includes(el.id))
+  //     // parsedData.splice(neededEl, 1)
+  //     console.log("Содержит")
+  // }
 
+  //    if (+e.target.parentElement.id === el.id) {
+  //             // parsedData.splice(neededEl, 1)
+  //        return
+  //       console.log(+e.target.parentElement.id === elem.id)
+  //         }
+  //    } )
+  //     console.log(elem)
+  //
+  //
 
-cardOpenModal.addEventListener("click", defineMovie)
+  // })
 
+  // e.target.textContent = 'Remove from "Watched"'
+  // +e.target.parentElement.id !== i.id
+}
+
+cardOpenModal.addEventListener('click', defineMovie);
 
 function addToWatchedArray(e) {
-    e.preventDefault()
-    fetchMovies(550).then(data => {
-        console.log(e)
-        data.results.forEach(elem => {
-            if (+e.srcElement.parentElement.attributes.value.value === elem.id) {
-                try {
-                    if (e.target.textContent === 'Remove from "Watched"') {
-                        removeFromArray('Watched')
-                        onModalClose
-                        return
-                    }
-                    const savedData = localStorage.getItem('Watched')
-                    const parsedData = JSON.parse(savedData)
+  e.preventDefault();
+  fetchMovies(550).then(data => {
+    console.log(e);
+    data.results.forEach(elem => {
+      if (+e.srcElement.parentElement.attributes.value.value === elem.id) {
+        try {
+          if (e.target.textContent === 'Remove from "Watched"') {
+            removeFromArray('Watched');
+            onModalClose;
+            return;
+          }
+          const savedData = localStorage.getItem('Watched');
+          const parsedData = JSON.parse(savedData);
 
-                    e.target.textContent = 'Remove from "Watched"'
+          e.target.textContent = 'Remove from "Watched"';
 
-                    parsedData.push(elem)
-                    localStorage.setItem('Watched', JSON.stringify(parsedData))
-
-
-
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-
-
-        })
-
-    })
+          parsedData.push(elem);
+          localStorage.setItem('Watched', JSON.stringify(parsedData));
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+  });
 }
 
 function addToQueuedArray(e) {
-    fetchMovies(550).then(data => {
-        data.results.forEach(elem => {
-            if (+e.srcElement.parentElement.attributes.value.value === elem.id) {
-                try {
-                    if (e.target.textContent === 'Remove from "Queued"') {
-                        removeFromArray('Queued')
-                        onModalClose
-                        return
-                    }
+  fetchMovies(550).then(data => {
+    data.results.forEach(elem => {
+      if (+e.srcElement.parentElement.attributes.value.value === elem.id) {
+        try {
+          if (e.target.textContent === 'Remove from "Queued"') {
+            removeFromArray('Queued');
+            onModalClose;
+            return;
+          }
 
-                    const savedData = localStorage.getItem('Queued')
-                    const parsedData = JSON.parse(savedData)
-                    e.target.textContent = 'Remove from "Queued"'
-                    parsedData.push(elem)
-                    localStorage.setItem('Queued', JSON.stringify(parsedData))
-
-
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-
-
-        })
-
-    })
+          const savedData = localStorage.getItem('Queued');
+          const parsedData = JSON.parse(savedData);
+          e.target.textContent = 'Remove from "Queued"';
+          parsedData.push(elem);
+          localStorage.setItem('Queued', JSON.stringify(parsedData));
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+  });
 }
 
 // function removeFromArray(string) {
@@ -185,31 +152,17 @@ function addToQueuedArray(e) {
 //                     localStorage.setItem(string, JSON.stringify(parsedData))
 // }
 
-
 function getButtonWatched() {
-    const buttonWatched = document.querySelectorAll('[data-button="Watched"]');
-    buttonWatched.forEach(elem => elem.addEventListener('click', addToWatchedArray))
+  const buttonWatched = document.querySelectorAll('[data-button="Watched"]');
+  buttonWatched.forEach(elem => elem.addEventListener('click', addToWatchedArray));
 }
 function getButtonQueued() {
-    const buttonQueued = document.querySelectorAll('[data-button="Queued"]');
-    buttonQueued.forEach(elem => elem.addEventListener('click', addToQueuedArray))
+  const buttonQueued = document.querySelectorAll('[data-button="Queued"]');
+  buttonQueued.forEach(elem => elem.addEventListener('click', addToQueuedArray));
 }
-
-
 
 // function getClickedElem() {
 //        const movieWrapper = document.querySelectorAll('.movie-wrapper')
 //     console.log(movieWrapper)
 //     movieWrapper.forEach(elem => elem.addEventListener('click', addMovieToWatched))
 // }
-
-
-
-
-
-
-
-
-
-
-
