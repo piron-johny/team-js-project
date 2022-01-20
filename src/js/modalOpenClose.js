2df4fdfc191d10cc397afaa6c4d7e8a810578ffb
Vitalii-Dzoma <vitoos900@gmail.com>
2022-01-20 16:20:38 +0200

    РАбота с динамикой модалки

--- src/js/modalOpenClose.js
+++ src/js/modalOpenClose.js
@@ -2,7 +2,7 @@ import modalRender from '../hbs/modalRender.hbs';
 
 const btnCloseModal = document.querySelector("[data-modal-close]");
 const backdropModalEl = document.querySelector("[data-backdrop]");
-const cardOpenModal = document.querySelector(".section-movies__set");
+export const cardOpenModal = document.querySelector(".section-movies__set");
 const modalEl = document.querySelector('.modal__card');
 
 
