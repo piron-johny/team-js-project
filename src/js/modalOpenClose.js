951933891baeb67d33664a4deb2d2e4217400d17
varunyk1805 <varunyk1805@gmail.com>
2022-01-21 15:48:44 +0200

    add changes for openModal

--- src/js/modalOpenClose.js
+++ src/js/modalOpenClose.js
@@ -1,4 +1,5 @@
 import modalRender from '../hbs/modalRender.hbs';
+import { initialData } from "./initialData";
 
 const body = document.querySelector('body');
 const btnCloseModal = document.querySelector('[data-modal-close]');
@@ -14,66 +15,67 @@ backdropModalEl.addEventListener('click', onBackdropClick);
 window.addEventListener('keydown', onEscPress);
 
 function onModalOpen(e) {
-console.log(e.target.parentElement)
-  if (e.target.parentElement.className === 'section-movies__card' ||
-    e.target.parentElement.className === 'movies-card__img-thumb' ||
-    e.target.parentElement.className === 'card-info__detals' ||
-    e.target.parentElement.className === 'card-info' ||
-    e.target.parentElement.className === 'movies-card__link') {
-        backdropModalEl.classList.remove("is-hidden")
-        body.style.overflow = 'hidden'
+  let target = e.target;
+  const currentTarget = e.currentTarget;
+
+  const temp = (target, currentTarget) => {
+    if (target.parentElement === currentTarget) {
+      backdropModalEl.classList.remove("is-hidden");
+      body.style.overflow = 'hidden';
           
-    const findId = +e.target.parentElement.parentElement.parentElement.id
-    console.dir(findId)
-        const savedFilms = JSON.parse(localStorage.getItem(`Trending`));
-        const modalFilm = savedFilms.find(film => film.id === +findId)
+      const findId = +target.id;
+      console.dir(findId);
+      const modalFilm = initialData.moviesArrayCurrent.find(film => film.id === findId);
+      modalEl.insertAdjacentHTML('beforeend', modalRender(modalFilm));
 
-        modalEl.insertAdjacentHTML('beforeend', modalRender(modalFilm));
+      const btnWatched = document.querySelector('[data-modal-watched]');
+      const btnQueued = document.querySelector('[data-modal-queue]');
+      btnWatched.addEventListener("click", () => {
+        const savedData = localStorage.getItem('Watched');
+        const parsedData = JSON.parse(savedData);
+       
+        if (parsedData.length === 0) {
+          parsedData.push(modalFilm);
+          btnWatched.textContent = 'Remove from "Watched"';
+          localStorage.setItem('Watched', JSON.stringify(parsedData));
+          savedData;
+          parsedData;
+          if (btnWatched.textContent === 'Remove from "Watched"') {
+            btnWatched.addEventListener("click", () => {
+              parsedData.splice(0, 1);
+              localStorage.setItem('Watched', JSON.stringify(parsedData));
+              onModalClose();
+            });
+          };
+        };
+      });
+      btnQueued.addEventListener("click", () => {
+        const savedData = localStorage.getItem('Queued');
+        const parsedData = JSON.parse(savedData);
+       
+        if (parsedData.length === 0) {
+          parsedData.push(modalFilm);
+          btnQueued.textContent = 'Remove from "Queued"';
+          localStorage.setItem('Queued', JSON.stringify(parsedData));
+          savedData;
+          parsedData;
+          if (btnQueued.textContent === 'Remove from "Queued"') {
+            btnQueued.addEventListener("click", () => {
+              parsedData.splice(0, 1);
+              localStorage.setItem('Queued', JSON.stringify(parsedData));
+              onModalClose();
+            });
+          };
+        };
+      });
+      return;
+    };
+    target = target.parentElement;
+    temp(target, currentTarget);
+  };
 
-        const btnWatched = document.querySelector('[data-modal-watched]')
-        const btnQueued = document.querySelector('[data-modal-queue]')
-        btnWatched.addEventListener("click", () => {
-            const savedData = localStorage.getItem('Watched')
-            const parsedData = JSON.parse(savedData)
-       
-            if (parsedData.length === 0) {
-                parsedData.push(modalFilm)
-                btnWatched.textContent = 'Remove from "Watched"'
-                localStorage.setItem('Watched', JSON.stringify(parsedData))
-                savedData;
-                parsedData;
-            if (btnWatched.textContent === 'Remove from "Watched"') {
-                btnWatched.addEventListener("click", () => {
-                    parsedData.splice(0, 1)
-                    localStorage.setItem('Watched', JSON.stringify(parsedData))
-                    onModalClose()
-                })
-              }
-            } 
-        })
-        btnQueued.addEventListener("click", () => {
-            const savedData = localStorage.getItem('Queued')
-            const parsedData = JSON.parse(savedData)
-       
-           if (parsedData.length === 0) {
-                parsedData.push(modalFilm)
-                btnQueued.textContent = 'Remove from "Queued"'
-                localStorage.setItem('Queued', JSON.stringify(parsedData))
-                savedData;
-                parsedData;
-            if (btnQueued.textContent === 'Remove from "Queued"') {
-                btnQueued.addEventListener("click", () => {
-                    parsedData.splice(0, 1)
-                    localStorage.setItem('Queued', JSON.stringify(parsedData))
-                    onModalClose()
-                })
-            }
-    
-            }
-        
-        })
-    }
-}
+  temp(target, currentTarget);
+};
 
 function onModalClose() {
   backdropModalEl.classList.add('is-hidden');
