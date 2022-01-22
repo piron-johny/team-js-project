5c65017195172ed625c2fdfecdb47e2a8f8ddcd2
Oleksii Ostafiichuk <issimo333@gmail.com>
2022-01-22 17:08:57 +0200

    up

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import './sass/main.scss';
 // import './js/form-login';
-// import './js/pagination.js';
+import './js/pagination.js';
 import './js/movies';
 import './js/watched_queued_movies';
 import './js/header';
 import './js/switch_Home&Library';
-// import './js/trendingMovies';
+import './js/trendingMovies';
 import './js/creadCard';
-import './js/modalOpenClose';
+// import './js/modalOpenClose';
 import './js/searchMovies';
 import './js/initialData';
-
 
 // DATA_URL = 'https://team-project-1da18-default-rtdb.europe-west1.firebasedatabase.app/user.json';  // Путь к данным пользователя в базе данных
 // const KEY = '2cf91cf1fed5026ae9524dc97ad33068';  // Ключ доступа к API фильмов
