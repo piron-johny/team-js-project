5854a36d7ad1fe02d93f43a5742a7c69c4e91e24
Oleksii Ostafiichuk <issimo333@gmail.com>
2022-01-24 18:37:39 +0200

    прихована пагінація бібліотеки
    якщо менше 20 фільмів у бібліотеці то пагінація прихована, якщо більше також прихована), ще в розробці

--- src/js/switch_Home&Library.js
+++ src/js/switch_Home&Library.js
@@ -9,20 +9,21 @@ const queuedBtn = document.querySelector('[name="queue"]');
 
 const moviesSet = document.querySelector('.section-movies__set');
 const paginationEl = document.getElementById('pagination');
+// import Pagination from 'tui-pagination';
+// import 'tui-pagination/dist/tui-pagination.css';
 
 const renderMyLibarary = event => {
   event.preventDefault();
   moviesSet.innerHTML = moviesRender(initialData.moviesArrayWatched);
   renderWatchedFilmList();
   watchedBtn.classList.add('superactive');
-  paginationEl.classList.add('hidden');
+  initialData.pagination();
 };
 const renderHome = event => {
   event.preventDefault();
   initialData.page = 1;
   initialData.trendingMovies();
   paginationEl.classList.remove('hidden');
-  initialData.pagination();
 };
 
 logo.addEventListener('click', renderHome);
@@ -32,8 +33,23 @@ btnMyLibrary.addEventListener('click', renderMyLibarary);
 function renderWatchedFilmList() {
   moviesSet.innerHTML = '';
   let watchedFilmListFromLS = localStorage.getItem('filmsWatched');
-  if (watchedFilmListFromLS !== null && JSON.parse(watchedFilmListFromLS).length !== 0) {
+  if (
+    watchedFilmListFromLS !== null &&
+    JSON.parse(watchedFilmListFromLS).length !== 0 &&
+    JSON.parse(watchedFilmListFromLS).length <= 20
+  ) {
     moviesSet.insertAdjacentHTML('afterbegin', moviesRender(JSON.parse(watchedFilmListFromLS)));
+    paginationEl.classList.add('hidden');
+    console.log(JSON.parse(watchedFilmListFromLS).length);
+  } else if (
+    watchedFilmListFromLS !== null &&
+    JSON.parse(watchedFilmListFromLS).length !== 0 &&
+    JSON.parse(watchedFilmListFromLS).length >= 20
+  ) {
+    moviesSet.insertAdjacentHTML('afterbegin', moviesRender(JSON.parse(watchedFilmListFromLS)));
+    paginationEl.classList.add('hidden');
+    // paginationEl.classList.remove('hidden');
+    // initialData.pagination();
   } else if (watchedFilmListFromLS === null || JSON.parse(watchedFilmListFromLS).length === 0) {
     moviesSet.innerHTML = '';
     const listItem = document.createElement('li');
@@ -52,8 +68,22 @@ function renderQueuedFilmList() {
   watchedBtn.classList.remove('superactive');
   moviesSet.innerHTML = '';
   let queueFilmListFromLS = localStorage.getItem('filmsQueue');
-  if (queueFilmListFromLS !== null && JSON.parse(queueFilmListFromLS).length !== 0) {
+  if (
+    queueFilmListFromLS !== null &&
+    JSON.parse(queueFilmListFromLS).length !== 0 &&
+    JSON.parse(queueFilmListFromLS).length <= 20
+  ) {
     moviesSet.insertAdjacentHTML('afterbegin', moviesRender(JSON.parse(queueFilmListFromLS)));
+    paginationEl.classList.add('hidden');
+  } else if (
+    queueFilmListFromLS !== null &&
+    JSON.parse(queueFilmListFromLS).length !== 0 &&
+    JSON.parse(queueFilmListFromLS).length >= 20
+  ) {
+    moviesSet.insertAdjacentHTML('afterbegin', moviesRender(JSON.parse(queueFilmListFromLS)));
+    paginationEl.classList.add('hidden');
+    // paginationEl.classList.remove('hidden');
+    // initialData.pagination();
   } else if (queueFilmListFromLS === null || JSON.parse(queueFilmListFromLS).length === 0) {
     moviesSet.innerHTML = '';
     const listItem = document.createElement('li');
@@ -61,7 +91,9 @@ function renderQueuedFilmList() {
     listItem.textContent = 'You do not have to queue movies to watch. Add them.';
     moviesSet.append(listItem);
   }
+
   queuedBtn.classList.add('main__navigationLibraryButtonActive');
   watchedBtn.classList.remove('main__navigationLibraryButtonActive');
 }
+
 queuedBtn.addEventListener('click', renderQueuedFilmList);
